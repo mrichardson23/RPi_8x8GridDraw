fd1560c66716e8d0455aabfaaee369331bed1341
Matt Richardson <mrichardson23@gmail.com>
2016-02-11 15:53:10 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd ~
 git clone https://github.com/mrichardson23/RPi_8x8GridDraw.git
 cd RPi_8x8GridDraw
 mv Sense\ HAT\ Grid\ Editor.desktop ~/Desktop
+sudo mv /usr/share/icons/Adwaita/ /usr/share/icons/Adwaita.fix
