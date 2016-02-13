f171ca3158e1fe6df6d3bd3a8687520c851b3156
Matt Richardson <mrichardson23@gmail.com>
2016-02-12 20:37:48 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ git clone https://github.com/mrichardson23/RPi_8x8GridDraw.git
 cd RPi_8x8GridDraw
 mv Sense\ HAT\ Grid\ Editor.desktop ~/Desktop
 sudo mv /usr/share/icons/Adwaita/ /usr/share/icons/Adwaita.fix
-echo "Sense HAT Grid Editor installed. Double click the icon on the desktop to launch\n.You may now close this terminal window."
+echo "\n\nSense HAT Grid Editor installed. Double click the icon on the desktop to launch.\nYou may now close this terminal window."
